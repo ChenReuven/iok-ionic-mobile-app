a87bf86b90234af36f10165bc8aad6ac56c22657
Boris Litvinsky <borisl@wix.com>
2015-11-12 11:27:40 +0200

    Initial

--- www/js/app.js
+++ www/js/app.js
@@ -5,133 +5,90 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services', 'ngCordova'])
+angular.module('iok', ['ionic', 'ionic.service.core', 'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
 
+      /*  // kick off the platform web client
+       Ionic.io();
 
-    /*  // kick off the platform web client
-     Ionic.io();
+       // this will give you a fresh user or the previously saved 'current user'
+       var user = Ionic.User.current();
 
-     // this will give you a fresh user or the previously saved 'current user'
-     var user = Ionic.User.current();
+       // if the user doesn't have an id, you'll need to give it one.
+       if (!user.id) {
+       user.id = Ionic.User.anonymousId();
+       // user.id = 'your-custom-user-id';
+       }
 
-     // if the user doesn't have an id, you'll need to give it one.
-     if (!user.id) {
-     user.id = Ionic.User.anonymousId();
-     // user.id = 'your-custom-user-id';
-     }
+       //persist the user
+       user.save();*/
 
-     //persist the user
-     user.save();*/
-
-    // Push Notification
-    var push = new Ionic.Push({
-      "debug": true,
-      "onNotification": function(notification) {
-        var payload = notification.payload;
-        console.log(notification, payload);
-      },
-      "onRegister": function(data) {
-        console.log(data.token);
-      },
-      "pluginConfig": {
-        "ios": {
-          "badge": true,
-          "sound": true
+      // Push Notification
+      var push = new Ionic.Push({
+        "debug": true,
+        "onNotification": function (notification) {
+          var payload = notification.payload;
+          console.log(notification, payload);
         },
-        "android": {
-          "iconColor": "#f93c2b"
+        "onRegister": function (data) {
+          console.log(data.token);
+        },
+        "pluginConfig": {
+          "ios": {
+            "badge": true,
+            "sound": true
+          },
+          "android": {
+            "iconColor": "#f93c2b"
+          }
         }
+      });
+
+      var user = Ionic.User.current();
+
+      var callback = function (pushToken) {
+        console.log('Registered token:', pushToken.token);
+        user.addPushToken(pushToken);
+        user.save(); // you NEED to call a save after you add the token
       }
+
+      push.register(callback);
+      /*push.register(function(token) {
+       console.log("Device token:",token.token);
+       });*/
+
     });
-
-    var user = Ionic.User.current();
-
-    var callback = function (pushToken) {
-      console.log('Registered token:', pushToken.token);
-      user.addPushToken(pushToken);
-      user.save(); // you NEED to call a save after you add the token
-    }
-
-    push.register(callback);
-    /*push.register(function(token) {
-      console.log("Device token:",token.token);
-     });*/
-
-
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
   })
 
-  // Each tab has its own nav history stack:
+  .config(function ($stateProvider, $urlRouterProvider) {
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+      // setup an abstract state for the tabs directive
+      .state('main', {
+        url: '/main',
+        abstract: true,
+        templateUrl: 'templates/main.html'
+      });
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/main');
+
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
