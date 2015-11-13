9abdfb753ca26f93f57b02919211ac1284bda55d
Boris Litvinsky <borisl@wix.com>
2015-11-13 05:01:32 +0200

    Initial

---
+++ www/js/directives/contact.js
@@ -0,0 +1,23 @@
+function ContactDirective($timeout) {
+  return {
+    restrict: 'E',
+    scope: {
+      model: '=',
+      onClick: '&',
+      withUpdates: '@'
+    },
+    templateUrl: '/templates/contact.html',
+    link: function ($scope, $element) {
+      $scope.onContactSelected = function () {
+        $scope.onClick({contact: $scope.model});
+        $scope.animate = true;
+
+        $timeout(function () {
+          $scope.animate = false;
+        }, 500);
+      }
+    }
+  }
+}
+
+angular.module('iok').directive('contact', ContactDirective);
